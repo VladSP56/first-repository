ab7b049dc3118b39db74694ddf0aa430b4584e74
VladSP56 <parkvls@gmail.com>
2024-12-15 23:01:13 +0300

    zerocod-first

--- convert.py
+++ convert.py
@@ -4,11 +4,11 @@ def currency_converter():
 
     # Изменяем курс в зависимости от суммы
     if amount < 100:
-        exchange_rate = 0.8  # курс для суммы меньше 100
+        exchange_rate = 0.95  # курс для суммы меньше 100
     elif 100 <= amount < 500:
-        exchange_rate = 0.75  # курс для суммы от 100 до 500
+        exchange_rate = 0.9  # курс для суммы от 100 до 500
     else:
-        exchange_rate = 0.7  # курс для суммы 500 и выше
+        exchange_rate = 0.85  # курс для суммы 500 и выше
 
     # Конвертация суммы
     converted_amount = amount * exchange_rate
